2a986163923ae5b07689d7f7fbb08f17e7745fcb
Ivan <polyzay@gmail.com>
2021-02-22 18:23:57 +0300

    Updates

---
+++ precache-manifest.f1ac0854b71a6199e39b0e565b06a84c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473cec29651a80e62c92fdc6b870e013",
+    "url": "/mortgage-offer/index.html"
+  },
+  {
+    "revision": "705ba28222727cf7d9fa",
+    "url": "/mortgage-offer/static/css/main.904d9fb9.chunk.css"
+  },
+  {
+    "revision": "1659fd403249060a9c6c",
+    "url": "/mortgage-offer/static/js/2.68d681dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mortgage-offer/static/js/2.68d681dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705ba28222727cf7d9fa",
+    "url": "/mortgage-offer/static/js/main.9c12894c.chunk.js"
+  },
+  {
+    "revision": "c33a0356eeecd0eb86db",
+    "url": "/mortgage-offer/static/js/runtime-main.97a5fac0.js"
+  },
+  {
+    "revision": "55286ac73d276136be1442cba0b31d0d",
+    "url": "/mortgage-offer/static/media/LatoBold.55286ac7.eot"
+  },
+  {
+    "revision": "8f2be703781ed1f073f2d5eaea006304",
+    "url": "/mortgage-offer/static/media/LatoBold.8f2be703.ttf"
+  },
+  {
+    "revision": "a0ca2cecf74466e56d30ce73067707f4",
+    "url": "/mortgage-offer/static/media/LatoBold.a0ca2cec.woff"
+  },
+  {
+    "revision": "0b2967bca4315264b6d62b5d8cf2b509",
+    "url": "/mortgage-offer/static/media/LatoLight.0b2967bc.eot"
+  },
+  {
+    "revision": "88ed952df90974932cf540e13c9fb8d8",
+    "url": "/mortgage-offer/static/media/LatoLight.88ed952d.woff"
+  },
+  {
+    "revision": "c9064bf904221c68cbdaf2b237b18cb2",
+    "url": "/mortgage-offer/static/media/LatoLight.c9064bf9.ttf"
+  },
+  {
+    "revision": "8e6fce0d5b188c05f1676fc4155d2bc9",
+    "url": "/mortgage-offer/static/media/line.8e6fce0d.svg"
+  },
+  {
+    "revision": "529c2384734897d8e41fe6708bc6173b",
+    "url": "/mortgage-offer/static/media/man.529c2384.svg"
+  }
+]);
